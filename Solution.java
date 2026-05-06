517083af3ab2c345312037c34d132ac704b73b18
Azhar3301 <it24031@mbstu.ac.bd>
2026-05-06 10:49:18 +0600

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+        int sum=0,t=x;
+        while(t>0){
+            sum+=t%10;
+            t/=10;
+        }
+        if(x%sum==0)System.out.println("Harshad number");
+        else System.out.println("not a Harshad number");
+    }
+    
+}
